fdb7895ae89cc7066dc1b85ea02d4c9e65dc9370
Robert Važan <robert.vazan@tutanota.com>
2022-11-04 19:12:16 +0000

    Standard noexception-slf4j version

--- src/java.py
+++ src/java.py
@@ -95,6 +95,7 @@ def define_use(dependency):
 use_stagean = define_use('com.machinezoo.stagean:stagean:1.3.0')
 use_closeablescope = define_use('com.machinezoo.closeablescope:closeablescope:1.0.0')
 use_noexception = define_use('com.machinezoo.noexception:noexception:1.9.0')
+use_noexception_slf4j = define_use('com.machinezoo.noexception:noexception-slf4j:1.0.1')
 use_hookless = define_use('com.machinezoo.hookless:hookless:0.14.4')
 use_pushmode = define_use('com.machinezoo.pushmode:pushmode:0.8.2')
 use_pmsite = define_use('com.machinezoo.pmsite:pmsite:0.18.4')
