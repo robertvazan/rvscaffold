520e5d3e720bd78c511d6142bc34d3b915dcd7d1
Robert Važan <robert.vazan@tutanota.com>
2023-05-07 16:52:54 +0000

    Upgraded hookless-noexception, hookless-collections, and pmdata

--- src/rvscaffold.py
+++ src/rvscaffold.py
@@ -386,9 +386,11 @@ class Java(Repository):
     use_hookless = define_use('com.machinezoo.hookless:hookless:0.16.0')
     use_hookless_time = define_use('com.machinezoo.hookless:hookless-time:0.1.0')
     use_hookless_prefs = define_use('com.machinezoo.hookless:hookless-prefs:0.1.0')
+    use_hookless_noexception = define_use('com.machinezoo.hookless:hookless-noexception:0.1.0')
+    use_hookless_collections = define_use('com.machinezoo.hookless:hookless-collections:0.1.0')
     use_pushmode = define_use('com.machinezoo.pushmode:pushmode:0.8.3')
     use_pmsite = define_use('com.machinezoo.pmsite:pmsite:0.18.5')
-    use_pmdata = define_use('com.machinezoo.pmdata:pmdata:0.12.4')
+    use_pmdata = define_use('com.machinezoo.pmdata:pmdata:0.12.5')
 
     # commonly used libraries
     use_slf4j = define_use('org.slf4j:slf4j-api:1.7.32')
