92c1f9a688d668ba6db680a17a5db641dbc4c88b
Robert Važan <robert.vazan@tutanota.com>
2024-01-21 12:52:24 +0000

    Upgraded foxcache

--- src/rvscaffold.py
+++ src/rvscaffold.py
@@ -393,7 +393,7 @@ class Java(Repository):
     use_ladybugformatters = define_use('com.machinezoo.ladybugformatters:ladybugformatters:0.1.2')
     use_remorabindings = define_use('com.machinezoo.remorabindings:remorabindings:0.2.2')
     use_meerkatwidgets = define_use('com.machinezoo.meerkatwidgets:meerkatwidgets:0.2.1')
-    use_foxcache = define_use('com.machinezoo.foxcache:foxcache:0.1.5')
+    use_foxcache = define_use('com.machinezoo.foxcache:foxcache:0.1.6')
     use_pmdata = define_use('com.machinezoo.pmdata:pmdata:0.13.1')
 
     # commonly used libraries
